170ce9760d75f6d01774ab4e5cc77a4cb74e2fcd
sharweng <marbellasharwinjohn@gmail.com>
2025-02-14 09:13:48 +0800

    header toggle bg zindex fix

--- includes/header.php
+++ includes/header.php
@@ -1,33 +1,33 @@
-<nav class="site-header border border-bottom navbar navbar-expand-lg bg-body-tertiary px-3">
-  <div class="container-fluid container-lg">
-    <a class="navbar-brand fw-bold" href="/gravekeepercms/">GraveKeeper</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+<nav class="site-header border-bottom navbar navbar-expand-lg bg-body-tertiary px-0 mx-0">
+  <div class="container-fluid container-lg header-dropdown px-0">
+    <a class="navbar-brand fw-bold ms-5" href="/gravekeepercms/">GraveKeeper</a>
+    <button class="navbar-toggler me-5" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse justify-content-between" id="navbarSupportedContent">
       <ul class="navbar-nav mb-2 mb-lg-0">
-        <li class="nav-item">
+        <li class="nav-item ps-5">
           <a class="nav-link active" aria-current="page" href="/gravekeepercms/">Home</a>
         </li>
-        <li class="nav-item">
+        <li class="nav-item ps-5">
           <a class="nav-link" href="#">Link</a>
         </li>
-        <li class="nav-item dropdown">
+        <li class="nav-item  ps-5 dropdown">
           <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
             Dropdown
           </a>
-          <ul class="dropdown-menu">
+          <ul class="dropdown-menu me-5">
             <li><a class="dropdown-item" href="#">Action</a></li>
             <li><a class="dropdown-item" href="#">Another action</a></li>
             <li><hr class="dropdown-divider"></li>
             <li><a class="dropdown-item" href="#">Something else here</a></li>
           </ul>
         </li>
-        <li class="nav-item">
+        <li class="nav-item ps-5">
           <a class="nav-link disabled" aria-disabled="true">Disabled</a>
         </li>
       </ul>
-      <div class="d-flex align-items-center">
+      <div class="d-flex align-items-center ps-5 pb-3">
         <?php
           if(!isset($_SESSION['roleDesc'])){
               $_SESSION['roleDesc'] = "";
@@ -57,7 +57,7 @@
                   </div>";
               }else{
                 echo'<a id="login-link" href="/gravekeepercms/user/login.php" class="nav-link disabled a-darker-grey me-4 text-decoration-none me-0" aria-disabled="true">Login</a>
-                  <a id="register-link" href="/gravekeepercms/user/register.php" class="disabled btn btn-darker-grey" aria-disabled="true">Register</a>';
+                    <a id="register-link" href="/gravekeepercms/user/register.php" class="disabled btn btn-darker-grey" aria-disabled="true">Register</a>';
               }
         ?>
         
