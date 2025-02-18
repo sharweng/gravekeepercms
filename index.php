76c4ec863468353cd93ab364bab582f51c47b078
sharweng <marbellasharwinjohn@gmail.com>
2025-02-18 21:25:47 +0800

    main index update

--- index.php
+++ index.php
@@ -3,7 +3,6 @@
   include("includes/config.php");
   include('includes/header.php');
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -12,16 +11,17 @@
   <title>Home Page</title>
   <!-- BOOTSTRAP AND CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="includes/styles/style.css">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <style>
+        <?php include('includes/styles/style.css') ?>
+    </style>
 </head>
 <body>
   <h1>This is the body.</h1>
-  <a href="/gravekeepercms/mapping/" class="btn btn-darker-grey">Map</a>
+  <a href="/gravekeepercms/section/" class="btn btn-darker-grey">Section</a>
 </body>
 </html>
-
 <?php
   include("includes/footer.php");
 ?>
