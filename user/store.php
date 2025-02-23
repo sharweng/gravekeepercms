daffd6eb1f0960a9cf7618bc1008507e6e9b2bc4
sharweng <marbellasharwinjohn@gmail.com>
2025-02-24 03:31:25 +0800

    added code for profile management and delete

--- user/store.php
+++ user/store.php
@@ -1,12 +1,18 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
     session_start();
     include("../includes/config.php");
 
     $_SESSION['name'] = trim($_POST['name']);
     $_SESSION['phone'] = trim($_POST['phone']);
-    $_SESSION['email'] = trim($_POST['email']);
     $_SESSION['pass'] = trim($_POST['password']);
     $_SESSION['cpass'] = trim($_POST['confirmPass']);
+    if(isset($_POST['submit-update'])){
+
+    }else{
+        $_SESSION['email'] = trim($_POST['email']);
+    }
 
     if(isset($_POST['submit-register'])){
 
@@ -134,5 +140,126 @@
             header("Location: login.php");
         }
         
+    }
+
+    if(isset($_POST['submit-update'])){
+
+        if(empty($_POST['email'])){
+            $_SESSION['message'] = $_SESSION['message'].'Enter an email. <br>';
+            header("Location: profile.php");
+            exit();
+        }else{
+            $email = trim($_POST['email']);
+            if(!preg_match("/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/", $email)){
+                $_SESSION['message'] = $_SESSION['message'].'Enter a valid email. <br>';
+                header("Location: profile.php");
+                exit();
+            }
+        }
+
+        if(empty($_POST['name'])){
+            $_SESSION['message'] = $_SESSION['message'].'Enter a name. <br>';
+            header("Location: profile.php");
+            exit();
+        }else{
+            $name = trim($_POST['name']);
+            if(!preg_match("/^[A-Za-z' -]{2,50}$/", $name)){
+                $_SESSION['message'] = $_SESSION['message'].'Enter a valid name. <br>';
+                header("Location: profile.php");
+                exit();
+            }
+        }
+
+        if(empty($_POST['phone'])){
+            $_SESSION['message'] = $_SESSION['message'].'Enter a phone number. <br>';
+            header("Location: profile.php");
+            exit();
+        }else{
+            $phone = trim($_POST['phone']);
+            if(!preg_match("/^\d{11}$/", $phone)){
+                $_SESSION['message'] = $_SESSION['message'].'Enter an 11 digit phone number. <br>';
+                header("Location: profile.php");
+                exit();
+            }
+        }    
+    
+        if((isset($_POST['password'])&&!isset($_POST['confirmPass']))||(!isset($_POST['password'])&&isset($_POST['confirmPass']))){
+            $_SESSION['message'] = $_SESSION['message'].'Enter both passwords. <br>';
+            header("Location: profile.php");
+            exit();
+        }elseif($_POST['password']!=$_POST['confirmPass']){
+            $_SESSION['message'] = $_SESSION['message'].'Passwords do not match. <br>';
+            header("Location: profile.php");
+            exit();
+        }
+
+        if((preg_match("/^[A-Za-z' -]{2,50}$/", $name))&&(preg_match("/^\d{11}$/", $phone))
+        &&(preg_match("/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/", $email))){
+            echo 'test'.$_SESSION['email'].' '.$email;
+            if(strtolower($_SESSION['email']) != strtolower($email)){
+                $searchsql = "SELECT email FROM user WHERE LOWER(email) = LOWER(?)";
+                echo $searchsql;
+                $stmt = mysqli_prepare($conn, $searchsql);
+                mysqli_stmt_bind_param($stmt, "s", $email);
+                mysqli_stmt_execute($stmt);
+                $result = mysqli_stmt_get_result($stmt);
+
+                if (mysqli_num_rows($result) > 0) {  // If email already exists
+                    $_SESSION['message'] = 'Email is already registered. Please log in or use a different email to register. <br>';
+                    header("Location: profile.php");
+                    exit();
+                }
+            }
+
+            $role = 2;
+            $status = 1;
+
+            $sql = "UPDATE user SET email = '$email', name = '$name', phone = '$phone'";
+
+            if(!empty($_POST['password'])&&!empty($_POST['confirmPass'])){
+                $pass = trim($_POST['password']);
+                if(!preg_match("/^.{12,}$/", $pass)){
+                    $_SESSION['message'] = $_SESSION['message'].'Password must be atleast 12 characters. <br>';
+                    header("Location: /gravekeepercms/user/profile.php");
+                    exit();
+                }
+                $password = sha1($pass);
+
+                $sql = $sql . ", password = '$password'";
+            }
+            echo $sql;
+            $sql = $sql . " WHERE user_id = " . $_SESSION['user_id'];
+            $result = mysqli_query($conn, $sql);
+            if($result){
+                echo'test';
+                $_SESSION['lname'] = '';
+                $_SESSION['fname'] = '';
+                $_SESSION['email'] = '';
+                $_SESSION['pass'] = '';
+                $_SESSION['cpass'] = '';
+                $_SESSION['add'] = '';
+                $_SESSION['phone'] = '';
+
+
+                $_SESSION['success'] = "Profile Saved.";
+                header("Location: /gravekeepercms/user/profile.php");
+                
+            }
+        }else{
+            header("Location: profile.php");
+            exit();
+        }
+    }
+
+    if(isset($_POST['submit-delete'])){
+        $sql = "DELETE FROM user WHERE user_id = ?";
+        $stmt = mysqli_prepare($conn, $sql);
+        mysqli_stmt_bind_param($stmt, "s", $_SESSION['user_id']);
+        $delete_result = mysqli_stmt_execute($stmt);
+        mysqli_stmt_close($stmt);
+
+        if ($delete_result) {
+            header("Location: logout.php");
+        } 
     }
 ?>
